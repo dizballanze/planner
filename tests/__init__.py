37b3eb071c01d1f03ff010632ad3fcfba64207fe
dizballanze <dizballanze@gmail.com>
2014-11-17 14:08:33 +0300

    tests: rectangle frame drawing

---
+++ tests/__init__.py
@@ -0,0 +1,24 @@
+from unittest import TestCase
+
+
+class BaseTestCase(TestCase):
+
+    """
+    Abstract base Test Case with additional asserts and common useful methods.
+    """
+
+    def assertLength(self, seq, length):
+        """ Assert that length of specified sequence is equal to specified number """
+        if len(seq) != length:
+            self.fail("Sequence length is {} but expected length is {}".format(len(seq), length))
+
+    def assertAttrib(self, svg_obj, attrib, value):
+        """
+        Check that specified attribute of svg object is has specified value.
+        """
+        if svg_obj.attribs[attrib] != value:
+            message = "SVG object has attribute {attr_name}={attr_value}, but expected value is {expected_value}".format(
+                attr_name=attrib,
+                attr_value=svg_obj.attribs[attrib],
+                expected_value=value)
+            self.fail(message)
--- tests/tests_frame_draw.py
+++ tests/tests_frame_draw.py
@@ -1,10 +1,41 @@
-from unittest import TestCase
+from tests import BaseTestCase
 
 
-class TestDrawFrame(TestCase):
+class TestFrame(BaseTestCase):
 
     """ Functional test of drawing frames with coordinates """
 
-    def test_foo(self):
-        """ Sample test """
-        self.assertTrue(True)
+    pass
+
+
+class TestRectFrame(BaseTestCase):
+
+    """ Test of rectangle frame """
+
+    CORNER = (10, 20)  # Corner coordinates
+    SIZES = (350, 250)  # Sizes (WIDTH x HEIGHT)
+    WALL_WIDTH = 25
+
+    @classmethod
+    def setUpClass(cls):
+        from planner.frame import RectFrame
+        cls.RectFrame = RectFrame
+
+    def test_draw(self):
+        """ Test frame drawing. Should create correct SVG objects. """
+        rect_frame = self.RectFrame(self.CORNER[0], self.CORNER[1], self.SIZES[0], self.SIZES[1], self.WALL_WIDTH)
+        svg_objects = rect_frame._draw()
+        self.assertLength(svg_objects, 2)
+        outer, inner = svg_objects
+        from svgwrite import shapes, mm
+        self.assertIsInstance(outer, shapes.Rect)
+        self.assertIsInstance(inner, shapes.Rect)
+        # Check sizes and coordinates
+        self.assertAttrib(outer, 'x', self.CORNER[0] * mm)
+        self.assertAttrib(outer, 'y', self.CORNER[1] * mm)
+        self.assertAttrib(outer, 'width', self.SIZES[0] * mm)
+        self.assertAttrib(outer, 'height', self.SIZES[1] * mm)
+        self.assertAttrib(inner, 'x', (self.CORNER[0] + self.WALL_WIDTH) * mm)
+        self.assertAttrib(inner, 'y', (self.CORNER[1] + self.WALL_WIDTH) * mm)
+        self.assertAttrib(inner, 'width', (self.SIZES[0] - 2 * self.WALL_WIDTH) * mm)
+        self.assertAttrib(inner, 'height', (self.SIZES[1] - 2 * self.WALL_WIDTH) * mm)
