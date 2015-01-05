e00b4a56744a45b4b04554bb50f0dfaf14c3cc3b
dizballanze <dizballanze@gmail.com>
2015-01-05 22:03:30 +0300

    feature: tiny linear dimenion with extension lines

--- planner/frame/dimension.py
+++ planner/frame/dimension.py
@@ -23,13 +23,16 @@ class BaseDimension(object):
         self.attribs.update(attribs)
         self.label_attribs = label_attribs or {}
 
-    def _get_middle_point(self, start_point, end_point, distance):
+    def _get_middle_point(self, start_point, end_point, distance=None):
         """
         Calculate point on line specified with 2 points on specified distance.
+        If `distance == None` it calculated as `length / 2`
         """
         x1, y1 = start_point
         x2, y2 = end_point
         line_length = ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
+        if distance is None:
+            distance = line_length / 2
         directing_vector = (x2 - x1, y2 - y1)
         unit_vector = (directing_vector[0] / line_length, directing_vector[1] / line_length)
         middle_point = (x1 + unit_vector[0] * distance, y1 + unit_vector[1] * distance)
@@ -160,4 +163,66 @@ class ExtensionableLinearDimension(BaseDimension):
         res.append(self._create_arrow(end_dimension_point, start_dimension_point, end_arrow_point))
         # Draw text
         res.append(self._render_text(start_dimension_point, end_dimension_point))
+        return res
+
+
+class TinyExtensionableLinearDimension(ExtensionableLinearDimension):
+
+    """
+    Tiny linear dimension, label located on the outter part of 
+        elongate dimension line.
+    """
+
+    def __init__(self, start_point, end_point, label, font=None, direction=1, extension_size=12, label_position='start', elongation=15, **attribs):
+        """
+        `direction` argument sets extension lines direction:
+            direction >= 0 - extension lines in coordinates increase direction
+            direction < 0 - extension lines in coordinates decrease direction
+        `label_position` - define position of label on elongate dimension line:
+            label_position == 'start' - label located close to start_point
+            label_position != 'start' - label located close to end_point
+        """
+        super(TinyExtensionableLinearDimension, self).__init__(start_point, end_point, label, font, direction, extension_size, **attribs)
+        self._start_position = label_position == 'start'
+        self.elongation = elongation
+
+    def _draw(self):
+        res = []
+        # Draw lines
+        unit_vector = self._get_perpendicular_unit_vector(self.start_point, self.end_point, self.start_point)
+        # inverse unit vector if needed
+        if self._direction:
+            unit_vector = [-part for part in unit_vector]
+        start_extension_point = (self.start_point[0] + unit_vector[0] * self.extension_size,
+                                 self.start_point[1] + unit_vector[1] * self.extension_size)
+        end_extension_point = (self.end_point[0] + unit_vector[0] * self.extension_size,
+                                 self.end_point[1] + unit_vector[1] * self.extension_size)
+        start_extension_line = shapes.Line(self.start_point, start_extension_point, **self.attribs)
+        end_extension_line = shapes.Line(self.end_point, end_extension_point, **self.attribs)
+        dimension_size = self.extension_size - self.EXTENSION_TAIL
+        start_dimension_middle_point = (self.start_point[0] + unit_vector[0] * dimension_size,
+                                 self.start_point[1] + unit_vector[1] * dimension_size)
+        end_dimension_middle_point = (self.end_point[0] + unit_vector[0] * dimension_size,
+                                 self.end_point[1] + unit_vector[1] * dimension_size)
+        if self._start_position:
+            start_dimension_point = self._get_middle_point(start_dimension_middle_point, end_dimension_middle_point, -(self.elongation + self.ARROW_LENGTH))
+            end_dimension_point = self._get_middle_point(end_dimension_middle_point, start_dimension_middle_point, -(self.ARROW_LENGTH + 2))
+        else:
+            end_dimension_point = self._get_middle_point(end_dimension_middle_point, start_dimension_middle_point, -(self.elongation + self.ARROW_LENGTH))
+            start_dimension_point = self._get_middle_point(start_dimension_middle_point, end_dimension_middle_point, -(self.ARROW_LENGTH + 2))
+        dimension_line = shapes.Line(start_dimension_point, end_dimension_point, **self.attribs)
+        res += [start_extension_line, end_extension_line, dimension_line]
+        # Draw arrows
+        start_arrow_point = self._get_middle_point(start_dimension_middle_point, start_dimension_point, self.ARROW_LENGTH)
+        end_arrow_point = self._get_middle_point(end_dimension_middle_point, end_dimension_point, self.ARROW_LENGTH)
+        res.append(self._create_arrow(start_dimension_middle_point, start_dimension_point, start_arrow_point))
+        res.append(self._create_arrow(end_dimension_middle_point, end_dimension_point, end_arrow_point))
+        # Draw text
+        if self._start_position:
+            start_text_point = start_dimension_point
+            end_text_point = start_arrow_point
+        else:
+            start_text_point = end_arrow_point
+            end_text_point = end_dimension_point
+        res.append(self._render_text(start_text_point, end_text_point))
         return res
